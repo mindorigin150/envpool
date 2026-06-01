5ed101215e6bee4c84cb7043d0b17d85a22e0923
Xinyuan Li <xinyuanli2327@outlook.com>
2026-06-01 18:28:08 +0800

    delete irrelevant installation

--- envpool/entry.py
+++ envpool/entry.py
@@ -13,42 +13,4 @@
 # limitations under the License.
 """Entry point for all envs' registration."""
 
-try:
-  import envpool.atari.registration  # noqa: F401
-except ImportError:
-  pass
-
-try:
-  import envpool.box2d.registration  # noqa: F401
-except ImportError:
-  pass
-
-try:
-  import envpool.classic_control.registration  # noqa: F401
-except ImportError:
-  pass
-
-try:
-  import envpool.mujoco.dmc.registration  # noqa: F401
-except ImportError:
-  pass
-
-try:
-  import envpool.mujoco.gym.registration  # noqa: F401
-except ImportError:
-  pass
-
-try:
-  import envpool.procgen.registration  # noqa: F401
-except ImportError:
-  pass
-
-try:
-  import envpool.toy_text.registration  # noqa: F401
-except ImportError:
-  pass
-
-try:
-  import envpool.vizdoom.registration  # noqa: F401
-except ImportError:
-  pass
+import envpool.vizdoom.registration  # noqa: F401
